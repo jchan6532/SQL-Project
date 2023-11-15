1bf432c7abd8b209cc895565b2965c03a88c1524
jchan6532 <82909011+jchan6532@users.noreply.github.com>
2023-11-15 00:50:43 -0500

    created view model

---
+++ FogLampSystem/ConfigurationTool/ViewModel/ConfigurationOptions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConfigurationTool.ViewModel
+{
+    public class ConfigurationOptions
+    {
+        public ConfigurationOptions()
+        {
+            // Setup config directory
+
+
+        }
+
+        internal void CreateConfigurationFile()
+        {
+            // Get XML service
+        }
+    }
+}
