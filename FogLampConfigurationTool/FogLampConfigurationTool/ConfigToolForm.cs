b959cf9965fec2d68d4c8afe9040318c95532942
jchan6532 <82909011+jchan6532@users.noreply.github.com>
2023-11-20 21:15:21 -0500

    added commenting

--- FogLampConfigurationTool/FogLampConfigurationTool/ConfigToolForm.cs
+++ FogLampConfigurationTool/FogLampConfigurationTool/ConfigToolForm.cs
@@ -1,7 +1,7 @@
 ﻿/*
 * FILE : ConfigToolForm.cs
-* PROJECT : PROG3070 - Gerritt Hooyer
-* PROGRAMMER : Gerritt Hooyer
+* PROJECT : PROG3070 - Gerritt Hooyer, Justin Chan
+* PROGRAMMER : Gerritt Hooyer, Justin Chan
 * FIRST VERSION : 2023-11-20
 * DESCRIPTION :
 * Adds functionality to the form for the config tool.
@@ -18,33 +18,64 @@ using System.Windows.Forms;
 
 namespace FogLampConfigurationTool
 {
+    /// <summary>
+    /// The class that encapsulates the form for the configuration tool form
+    /// </summary>
     public partial class ConfigToolForm : Form
     {
+        #region Constructors
+
+        /// <summary>
+        /// Default constructor
+        /// </summary>
         public ConfigToolForm()
         {
             InitializeComponent();
         }
 
+        #endregion
+
+        #region Private Methods
+
+        /// <summary>
+        /// Loads the configuration tool form on start up
+        /// </summary>
+        /// <param name="sender">object that invoked the event</param>
+        /// <param name="e">event arguments</param>
         private void ConfigToolForm_Load(object sender, EventArgs e)
         {
             UpdateDataGridView();
         }
 
+        /// <summary>
+        /// Method to call whenever a new update of the SQL server database data is required for the data grid 
+        /// </summary>
         private void UpdateDataGridView()
         {
             configTableDataGridView.DataSource = DatabaseManager.GetConfigData();
         }
 
-
+        /// <summary>
+        /// Refreshes the data grid with the latest data from the SQL server database
+        /// </summary>
+        /// <param name="sender">object that invoked the event</param>
+        /// <param name="e">event arguments</param>
         private void refreshButton_Click(object sender, EventArgs e)
         {
             UpdateDataGridView();
         }
 
+        /// <summary>
+        /// Saves the data on the data grid into the SQL server database
+        /// </summary>
+        /// <param name="sender">object that invoked the event</param>
+        /// <param name="e">event arguments</param>
         private void saveButton_Click(object sender, EventArgs e)
         {
             DatabaseManager.UpdateConfigData((DataTable)configTableDataGridView.DataSource);
             UpdateDataGridView();
         }
+
+        #endregion
     }
 }
