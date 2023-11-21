395285b78a5c6bf6e96ca9f8cd8fdea7cb1546ed
jchan6532 <82909011+jchan6532@users.noreply.github.com>
2023-11-21 03:56:01 -0500

    fixed datatable issue

--- FogLampSystem/ConfigurationTool/MainWindow.xaml.cs
+++ FogLampSystem/ConfigurationTool/MainWindow.xaml.cs
@@ -16,9 +16,9 @@ using System.Windows.Shapes;
 using System.Xml;
 
 using ConfigurationTool.ViewModel;
-using ConfigurationTool.Model;
 using System.Data;
 using System.Collections.ObjectModel;
+using System.Data.SqlClient;
 
 namespace ConfigurationTool
 {
@@ -27,25 +27,86 @@ namespace ConfigurationTool
     /// </summary>
     public partial class MainWindow : Window
     {
-        public bool ConfigurationKeyAdded { get; set; } = false;
+        private const string ConnectionString = @"Data Source=DESKTOP-UMCIMBB\SQLEXPRESS;Initial Catalog=SQL-PROJECT;Integrated Security=True";
         public MainWindow()
         {
             InitializeComponent();
-            DataContext = new ConfigurationViewModel();
+            // Load data from the database if needed
+            LoadData();
         }
 
         private void SaveBtn_Click(object sender, RoutedEventArgs e)
         {
-            var configs = (DataContext as ConfigurationViewModel).ConfigurationData;
-            var nullConfigs = configs.Where(configItem => configItem.ConfigurationKey == null);
-            nullConfigs.ToList().ForEach(nullConfig => configs.RemoveAt(configs.IndexOf(nullConfig)));
-
-            (DataContext as ConfigurationViewModel).UpdateData();
+            // Save changes to the database
+            SaveData();
         }
 
-        private void Window_Loaded(object sender, RoutedEventArgs e)
+        public void LoadData()
         {
-            (DataContext as ConfigurationViewModel).GetConfigurationData();
+
+            // Create a SQL command
+            string sql = "SELECT * FROM ConfigSettings";
+
+            // Create a connection
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
+            {
+                connection.Open();
+                // Create a command
+                using (SqlCommand command = new SqlCommand(sql, connection))
+                {
+                    // Create a data adapter
+                    using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+                    {
+                        adapter.SelectCommand = command;
+
+                        // Create a DataTable
+                        DataTable configSettingsTable = new DataTable();
+
+                        // Fill the DataTable with data from the database
+                        adapter.Fill(configSettingsTable);
+
+                        // Bind the DataTable to the DataGrid's ItemsSource
+                        configDataGrid.ItemsSource = configSettingsTable.DefaultView;
+                    }
+                }
+                connection.Close();
+            }
         }
+
+        // Implement the SaveChangesToDatabase method
+        public void SaveData()
+        {
+            // Create a connection
+            // Create SQL connection
+            SqlConnection conn = new SqlConnection(ConnectionString);
+
+            // Create SQL adapter
+            SqlDataAdapter adapter = new SqlDataAdapter($"SELECT config_key, int32_value, double_value, string_value FROM ConfigSettings", conn);
+
+            // Create SQL builder
+            SqlCommandBuilder builder = new SqlCommandBuilder(adapter);
+
+            // Setting the prefix and suffix for database objects
+            builder.QuotePrefix = "[";
+            builder.QuoteSuffix = "]";
+
+            adapter.UpdateCommand = builder.GetUpdateCommand(false);
+            adapter.DeleteCommand = builder.GetDeleteCommand(false);
+            adapter.InsertCommand = builder.GetInsertCommand(false);
+
+            // Open the connection
+            conn.Open();
+
+            DataTable table = ((DataView)configDataGrid.ItemsSource).Table;
+            //DataTable table = dataView.ToTable();
+
+
+            // Update the adapter with the data table
+            adapter.Update(table);
+
+            // Close the connection
+            conn.Close();
+        }
+
     }
 }
---
+++ FogLampSystem/ConfigurationTool/ViewModel/ConfigSettingsViewModel .cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Collections.ObjectModel;
+
+namespace ConfigurationTool.ViewModel
+{
+    public class ConfigSettingsViewModel : INotifyPropertyChanged
+    {
+        // Define properties for your ConfigSettings columns
+        private string _configKey;
+        public string ConfigKey
+        {
+            get { return _configKey; }
+            set
+            {
+                if (_configKey != value)
+                {
+                    _configKey = value;
+                    OnPropertyChanged(nameof(ConfigKey));
+                }
+            }
+        }
+
+        private int _int_value;
+        public int IntValue
+        {
+            get { return _int_value; }
+            set
+            {
+                if (_int_value != value)
+                {
+                    _int_value = value;
+                    OnPropertyChanged(nameof(IntValue));
+                }
+            }
+        }
+
+        private double _float_value;
+        public double FloatValue
+        {
+            get { return _float_value; }
+            set
+            {
+                if (_float_value != value)
+                {
+                    _float_value = value;
+                    OnPropertyChanged(nameof(FloatValue));
+                }
+            }
+        }
+
+        private string _string_value;
+        public string StringValue
+        {
+            get { return _string_value; }
+            set
+            {
+                if (_string_value != value)
+                {
+                    _string_value = value;
+                    OnPropertyChanged(nameof(StringValue));
+                }
+            }
+        }
+
+        // Add properties for other columns (int_value, float_value, string_value)
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+}
